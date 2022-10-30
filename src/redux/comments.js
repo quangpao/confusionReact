417b8b46958b2a10a72cd5c560e6f24ff3350daf
quangpao <ddquangbao@gmail.com>
2022-10-30 22:46:38 +0700

    Combining Reducers

---
+++ src/redux/comments.js
@@ -0,0 +1,8 @@
+import { COMMENTS } from '../shared/comments'
+
+export const Comment = (state = COMMENTS, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
